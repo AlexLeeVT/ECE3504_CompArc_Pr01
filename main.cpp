edef9e64622c593d1d956a162c4bc4c0de2d0678
Alexander Lee <95508161+AlexLeeVT@users.noreply.github.com>
2023-03-01 12:31:58 +0000

    start of decompiler

--- main.cpp
+++ main.cpp
@@ -2,12 +2,47 @@ using namespace std;
 
 #include "main.hpp"
 
+queue<int> readFile(string fileName) {
+    cout << "File: " << fileName << endl;
+    ifstream inFile(fileName);
+
+    queue<int> queue;
+    try {
+        while(!inFile.eof()) {
+            // get line from file
+            string hexnum;
+            inFile >> hexnum;
+
+            // convert hex to int
+            unsigned int x = 0;
+            stringstream ss;
+            ss << hex << hexnum;
+            ss >> x;
+
+            // put it into the queue
+            queue.push(static_cast<int>(x));
+        }
+    } catch(ifstream::failure e) {
+        cerr<< "Exception opening/reading/closing file\n";
+    }
+    inFile.close();
+
+    return queue;
+}
+
 int main(int argc, char** argv) {
-    cout << "You have entered " << argc
-         << " arguments:" << "\n";
-  
-    for (int i = 0; i < argc; i++)
-        cout << argv[i] << "\n";
-  
+    queue<int> queue = readFile(argv[1]);
+
+    while(!queue.empty()) {
+        int x = queue.front();
+        queue.pop();
+        cout << endl << x << endl;
+
+        cout << "func: " << (x & FUNC_BITMASK) << endl;
+        cout << "rs: " << ((x & RS_BITMASK) >> RS_BITSHIFT) << endl;
+        cout << "rt: " << ((x & RT_BITMASK) >> RT_BITSHIFT) << endl;
+        cout << "rd: " << ((x & RD_BITMASK) >> RD_BITSHIFT) << endl;
+    }
     return 0;
 }
+
--- main.hpp
+++ main.hpp
@@ -2,5 +2,28 @@
 #define MAIN_HPP
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <queue>
+#include <stdexcept>
+#include <sstream>
+
+#define OP_BITSHIFT     26
+#define RS_BITSHIFT     21
+#define RT_BITSHIFT     16
+#define RD_BITSHIFT     11
+#define SHAMT_BITSHIFT  6
+#define FUNC_BITSHIFT   0
+#define IMM_BITSHIFT    0
+#define ADDR_BITSHIFT   0
+
+#define OP_BITMASK      (63 << 26)
+#define RS_BITMASK      (31 << 21)
+#define RT_BITMASK      (31 << 16)
+#define RD_BITMASK      (31 << 11)
+#define SHAMT_BITMASK   (31 << 6)
+#define FUNC_BITMASK    (63)
+#define IMM_BITMASK     (65535)
+#define ADDR_BITMASK    (67108863)
 
 #endif
